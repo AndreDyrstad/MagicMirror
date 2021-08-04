8f86a96544c1f8b76f14e514d206f6484f945195
AndreDyrstad <dyrstad.a@gmail.com>
2021-08-04 12:38:59 +0200

    Some weather stuff

--- app/src/main/java/com/ady/magicmirror/fragment/WeatherFragment.kt
+++ app/src/main/java/com/ady/magicmirror/fragment/WeatherFragment.kt
@@ -5,12 +5,19 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.ady.magicmirror.R
+import com.ady.magicmirror.configurations.Weather.DISPLAY_HOURS
+import com.ady.magicmirror.configurations.Weather.LATITUDE
+import com.ady.magicmirror.configurations.Weather.LONGITUDE
 import com.ady.magicmirror.model.Weather
+import com.ady.magicmirror.util.APIService
 import com.android.volley.AuthFailureError
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import java.lang.StringBuilder
+import java.time.Instant
+import java.time.ZoneOffset
 import kotlin.collections.HashMap
 
 
@@ -22,16 +29,33 @@ class WeatherFragment : Fragment(R.layout.activity_weather) {
     }
 
     private fun requestWithSomeHttpHeaders(findViewById: TextView) {
+
         val gson = Gson()
         val queue = Volley.newRequestQueue(this.context)
-        val url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.39299&lon=5.32415"
+        val url =
+            "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=$LATITUDE&lon=$LONGITUDE"
         val getRequest: StringRequest = object : StringRequest(
             Method.GET, url,
-            Response.Listener { response -> // response
-                val data: Weather.WeatherData = gson.fromJson(response, Weather.WeatherData::class.java)
-                findViewById.text = data.properties.timeseries[0].data.instant.details.air_temperature.toString()
+            Response.Listener { response ->
+                val data: Weather.WeatherData =
+                    gson.fromJson(response, Weather.WeatherData::class.java)
+                val timeSeries = data.properties.timeseries
+                val t2 = timeSeries.filter {
+                    DISPLAY_HOURS.contains(Instant.parse(it.time).atZone(ZoneOffset.UTC).hour)
+                }
+
+                val sb = StringBuilder()
+
+                t2.forEach {
+                    sb.append(
+                        "${it.data.instant.details.air_temperature} " +
+                                "${it.data.next_1_hours?.summary?.symbol_code ?: "NaN"}\n"
+                    )
+                }
+
+                findViewById.text = sb.toString()
             },
-            Response.ErrorListener { error -> // TODO Auto-generated method stub
+            Response.ErrorListener { error ->
                 findViewById.text = error.toString()
             }
         ) {
---
+++ app/src/main/java/com/ady/magicmirror/fragment/WeatherIconFragment.kt
@@ -0,0 +1,7 @@
+package com.ady.magicmirror.fragment
+
+import androidx.fragment.app.Fragment
+import com.ady.magicmirror.R
+
+class WeatherIconFragment: Fragment(R.layout.activity_weather) {
+}
